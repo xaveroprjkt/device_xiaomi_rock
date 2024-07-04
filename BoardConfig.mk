61fd549b8c5c9e7305dae70e0ee094d3c5c1ec62
Marc Bourgoin <themard85@gmail.com>
2024-07-04 16:19:28 +0200

    rock: Fix audio on QPR3 with alsa audio
    As of Android 14 QPR3, our device requires the use of alsa audio,
    otherwise audio HAL keeps crashing with error messages of being
    unable to route audio and listAudioPorts failed
    
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,6 +25,9 @@ TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a76
 TARGET_CPU_VARIANT_RUNTIME := cortex-a76
 
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+
 # Enable 64-bit for non-zygote.
 ZYGOTE_FORCE_64 := true
 
