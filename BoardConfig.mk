61688220e8bd3e810862048ef55b039f0865c3c0
Christopher Ferris <cferris@google.com>
2024-08-26 23:44:15 +0100

    rock: Enable 32 bit building, but only 64 bit zygote.
    Bug: 229787331
    Test: Build/Boot in progress.
    Change-Id: I4686b567fd96a6cd7c3f6e269f0424811c0b3d6a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,6 +25,9 @@ TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a76
 TARGET_CPU_VARIANT_RUNTIME := cortex-a76
 
+# Enable 64-bit for non-zygote.
+ZYGOTE_FORCE_64 := true
+
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
