b2e3bfc5b5d92e77bcb6e13cee3512f1e519ff0d
xyzuniverse <dev.xyzuniverse@gmail.com>
2025-04-15 20:17:39 +0800

    rock: Drop `TARGET_CPU_ABI2`
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,6 @@ KERNEL_PATH := kernel/xiaomi/rock
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a76
 TARGET_CPU_VARIANT_RUNTIME := cortex-a76
 
