32265023f38862af72f01a2f47e6f511997cc6f1
ChrisCatto <chriscatto@proton.me>
2025-05-09 23:15:43 +0700

    rock: Silence FrameTracker logspam
    Change-Id: 176fd98533cff164e7b0be2f129de353db0f924b
    Signed-off-by: picasso09 <picasso09DV@proton.me>
    
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- config/prop/vendor_logtag.mk
+++ config/prop/vendor_logtag.mk
@@ -222,4 +222,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.log.tag.mtkpower_client=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.wpfa_iptable_android=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.gralloc4=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.hwcomposer=$(VENDOR_LOG_LEVEL)
+    persist.log.tag.hwcomposer=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.FrameTracker=$(VENDOR_LOG_LEVEL)
