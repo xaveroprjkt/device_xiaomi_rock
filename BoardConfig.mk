d8044dcf3522911233021ed1570ca2a55c88b542
ChrisCatto <chriscatto@proton.me>
2025-05-13 01:45:10 +0700

    rock: Inherit common Xiaomi framework compatibility matrix
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -230,6 +230,7 @@ BOARD_VNDK_VERSION := current
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
 	vendor/lineage/config/device_framework_matrix.xml \
+	hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
 	$(DEVICE_PATH)/config/vintf/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/config/vintf/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/config/vintf/compatibility_matrix.xml
