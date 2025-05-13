9423dd68e4930022b7f7c3380d9d7b58343a42a4
imnathanzero <nathannxx@proton.me>
2025-05-13 14:16:07 +0700

    Revert "rock: Enable 32 bit building, but only 64 bit zygote."
    This reverts commit 61688220e8bd3e810862048ef55b039f0865c3c0.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,9 +27,6 @@ TARGET_CPU_VARIANT_RUNTIME := cortex-a76
 # Audio
 BOARD_USES_ALSA_AUDIO := true
 
-# Enable 64-bit for non-zygote.
-ZYGOTE_FORCE_64 := true
-
 # Force any prefer32 targets to be compiled as 64 bit.
 IGNORE_PREFER32_ON_DEVICE := true
 
