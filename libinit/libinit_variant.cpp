c76905652a043949a6073c9ef98ca6b9dafeba95
picasso09 <picasso09DV@proton.me>
2025-10-20 08:47:36 +0000

    rock: init: fix logic expression search variant
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -15,7 +15,7 @@ using android::base::GetProperty;
 #define HWC_PROP "ro.boot.hwc"
 #define SKU_PROP "ro.boot.product.hardware.sku"
 
-void search_variant(const std::vector<variant_info_t> variants) {
+bool search_variant(const std::vector<variant_info_t> variants) {
     std::string hwc_value = GetProperty(HWC_PROP, "");
     std::string sku_value = GetProperty(SKU_PROP, "");
 
@@ -23,9 +23,10 @@ void search_variant(const std::vector<variant_info_t> variants) {
         if ((variant.hwc_value == "" || variant.hwc_value == hwc_value) &&
             (variant.sku_value == "" || variant.sku_value == sku_value)) {
             set_variant_props(variant);
-            break;
+            return true;
         }
     }
+ return false;
 }
 
 void set_variant_props(const variant_info_t variant) {
