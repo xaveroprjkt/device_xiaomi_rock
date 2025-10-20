8d5de68e42c886300a6ab8edbeafa37721dcfcbe
picasso09 <picasso09DV@proton.me>
2025-10-20 07:43:50 +0000

    setup: change target to fourteen branch
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,16 +1,16 @@
 echo -e "\033[1;33mCloning Dependencies\033[0m"
 
 # Kernel
-git clone https://github.com/picasso09/kernel_xiaomi_rock -b thirteen kernel/xiaomi/rock
+git clone https://github.com/picasso09/kernel_xiaomi_rock -b fourteen kernel/xiaomi/rock
 
 # Vendor
-git clone https://github.com/picasso09/proprietary_vendor_xiaomi_rock -b thirteen vendor/xiaomi/rock
+git clone https://github.com/picasso09/proprietary_vendor_xiaomi_rock -b fourteen vendor/xiaomi/rock
 
 # Hardware
-git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-20 hardware/mediatek
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-21 hardware/mediatek
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
 
 # Sepolicy
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-20 device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-21 device/mediatek/sepolicy_vndr
 
 echo -e "\033[32mDone go cook\033[0m"
