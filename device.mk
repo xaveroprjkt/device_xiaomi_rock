cf06b560242dd09dd020538d7f396547522f5787
vannxavero <vannxaveroo@proton.me>
2025-12-23 07:15:02 +0000

    rock: move to common usb service
    Signed-off-by: vannxavero <vannxaveroo@proton.me>

--- device.mk
+++ device.mk
@@ -434,8 +434,8 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3.vendor \
-    android.hardware.usb.gadget@1.1.vendor
+    android.hardware.usb-service.mediatek \
+    android.hardware.usb.gadget-service.mediatek
 
 # Vibrator
 PRODUCT_PACKAGES += \
