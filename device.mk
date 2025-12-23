bd8268180f7b08110a82fb22980ad5a6af8b79a4
vannxavero <vannxaveroo@proton.me>
2025-12-23 07:10:57 +0000

    rock: Use Common Boot Control
    Signed-off-by: vannxavero <vannxaveroo@proton.me>

--- device.mk
+++ device.mk
@@ -100,9 +100,8 @@ PRODUCT_COPY_FILES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service
+    com.android.hardware.boot \
+    android.hardware.boot-service.default_recovery
 
 PRODUCT_PACKAGES += \
     create_pl_dev \
